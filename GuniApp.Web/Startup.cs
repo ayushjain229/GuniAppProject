aaeab9f03f16dce5bd4aa447e992b0dc64bf365a
contact4manoj <mailme@manojkumarsharma.com>
2022-02-14 18:17:19 +0530

    Registered the Email Sender Service.  And Customized the Register Page.

--- GuniApp.Web/Startup.cs
+++ GuniApp.Web/Startup.cs
@@ -1,18 +1,15 @@
 using GuniApp.Web.Data;
 using GuniApp.Web.Models;
+using GuniApp.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GuniApp.Web
 {
@@ -72,6 +69,12 @@ namespace GuniApp.Web
                 });
 
             services.AddRazorPages();
+
+            // Register the Customized Email Sender Service 
+            // -- SMTP configuration is in the appsettings.json file.
+            services
+                .AddSingleton<IEmailSender, MyEmailSender>();
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
